639f563ef78d74b62d8075d39d89f4df3e72260a
Chahat Nathwani <nathwanichahat@gmail.com>
2025-10-26 00:18:46 +0530

    feat: configure Express app and integrate routes

---
+++ src/app.js
@@ -0,0 +1,9 @@
+//Express app setup
+import express from "express";
+import fileRoutes from "./routes/fileRoutes.js";
+
+const app = express();
+app.use(express.json());
+app.use("/api/files", fileRoutes);
+
+export default app;
